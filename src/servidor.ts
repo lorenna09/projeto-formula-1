1327076aef2cb49726c2d6539def33ac80e9793e
Lorena Castro <167568325+lorenna09@users.noreply.github.com>
2026-05-25 17:28:05 -0300

    Update servidor.ts

--- src/servidor.ts
+++ src/servidor.ts
@@ -7,30 +7,30 @@ server.register(cors, {
   origin: "*",
 });
 
-const teams = [
-  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
-  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
-  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
-  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
-  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
-  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
-  { id: 7, name: "Alfa Romeo Racing", base: "Hinwil, Switzerland" },
-  { id: 8, name: "AlphaTauri", base: "Faenza, Italy" },
-  { id: 9, name: "Williams", base: "Grove, United Kingdom" },
-  { id: 10, name: "Haas", base: "Kannapolis, United States" },
-  { id: 11, name: "Uralkali Haas F1 Team", base: "Banbury, United Kingdom" },
-  { id: 12, name: "Scuderia Toro Rosso", base: "Faenza, Italy" },
+const cars = [
+  { id: 1, name: "Lamborghini Aventador", country: "Italy" },
+  { id: 2, name: "Ferrari SF90", country: "Italy" },
+  { id: 3, name: "Bugatti Chiron", country: "France" },
+  { id: 4, name: "Porsche 911 Turbo", country: "Germany" },
+  { id: 5, name: "McLaren P1", country: "United Kingdom" },
+  { id: 6, name: "Nissan GT-R", country: "Japan" },
+  { id: 7, name: "Chevrolet Camaro", country: "United States" },
+  { id: 8, name: "Ford Mustang", country: "United States" },
+  { id: 9, name: "BMW M4", country: "Germany" },
+  { id: 10, name: "Audi R8", country: "Germany" },
+  { id: 11, name: "Toyota Supra", country: "Japan" },
+  { id: 12, name: "Koenigsegg Jesko", country: "Sweden" },
 ];
 
 const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-  { id: 2, name: "Lando Norris", team: "McLaren" },
+  { id: 1, name: "Brian O'Conner", car: "Nissan GT-R" },
+  { id: 2, name: "Dominic Toretto", car: "Dodge Charger" },
+  { id: 3, name: "Letty Ortiz", car: "Ford Mustang" },
 ];
 
-server.get("/teams", async (request, response) => {
+server.get("/cars", async (request, response) => {
   response.type("application/json").code(200);
-  return { teams };
+  return { cars };
 });
 
 server.get("/drivers", async (request, response) => {
@@ -51,13 +51,13 @@ server.get<{ Params: DriverParams }>(
     if (!driver) {
       response.type("application/json").code(404);
       return { message: "Driver Not Found" };
-    } else {
-      response.type("application/json").code(200);
-      return { driver };
     }
+
+    response.type("application/json").code(200);
+    return { driver };
   }
 );
 
 server.listen({ port: 3333 }, () => {
-  console.log("Server init");
+  console.log("🚗 Server running on port 3333");
 });
